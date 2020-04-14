4f47fd9e371eb0da1bf83d40720e62f77025ce76
PashkaBro <veenoompoltava@gmail.com>
2020-04-13 19:20:49 -0700

    Updates

---
+++ precache-manifest.510c28b22d0c1625caf94072515dda67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0d925532f845eb3ee19b76454880fb3",
+    "url": "/WeatherByCityName/index.html"
+  },
+  {
+    "revision": "9b61a7e155de14f6d990",
+    "url": "/WeatherByCityName/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "fb826195d4e2de72aaf0",
+    "url": "/WeatherByCityName/static/css/main.509a02cc.chunk.css"
+  },
+  {
+    "revision": "9b61a7e155de14f6d990",
+    "url": "/WeatherByCityName/static/js/2.7790ed2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherByCityName/static/js/2.7790ed2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb826195d4e2de72aaf0",
+    "url": "/WeatherByCityName/static/js/main.3479bd6b.chunk.js"
+  },
+  {
+    "revision": "1f4fd10f1e70470ea068",
+    "url": "/WeatherByCityName/static/js/runtime-main.868ea79d.js"
+  }
+]);
