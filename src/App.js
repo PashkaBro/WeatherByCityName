b9286e0b1dbcc10764267cd3f1a661914fa2555b
PashkaBro <veenoompoltava@gmail.com>
2020-04-13 19:23:49 -0700

    last commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import forecast from './Forecast'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+
+    const [todo, setTodo] = useState();
+    const [input, setInput] = useState();
+
+    function ClearFields() {
+        document.getElementById("textfield1").value = "";
+    }
+
+    function FormatedData(n) {
+        let res = n.data.main.temp - 273.15;
+        let cast = n.data.weather[0].main;
+        setTodo(
+            <div className='Resp'>
+                <div>  {`Current weather in ${input[0].toUpperCase() + input.substring(1, input.length)} :`}
+                </div>
+                {forecast[cast]}
+                {Math.round(res)}
+                <img width={50} height='auto' className="mr-3"
+                     src="https://cdn3.iconfinder.com/data/icons/meteocons/512/celsius-512.png"
+                     alt="Generic placeholder"/>
+            </div>);
+    }
+
+    const todoInput = (n) => {
+        setInput(n.target.value)
+    };
+
+    const axios = require('axios');
+
+    const getTodos = () => {
+
+        axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${input}&APPID=68ad0be2770fb1c404749a07ccaa8418`)
+            .then(function (response) {
+                FormatedData(response);
+                console.log(input);
+            })
+            .catch(function (error) {
+                setTodo(<div>
+                    Please enter real City
+                </div>);
+                console.log(error);
+            });
+
+        ClearFields()
+    };
+
+    return (
+
+        <div className="App">
+            <br/>
+            <input id='textfield1' type='text' autoFocus={true} placeholder='Enter city' onChange={todoInput}/>
+            <button type="button" className="btn btn-success" onClick={getTodos}> Get Weather</button>
+            {todo}
+        </div>
+
+    );
 }
 
 export default App;
---
+++ src/Forecast.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default {
+    Rain: <img width={50} height='auto' className="mr-3"
+               src="https://icons-for-free.com/iconfiles/png/128/forecast+rain+weather+icon-1320183300378577212.png" alt="Clear"/>,
+    Clear: <img width={50} height='auto' className="mr-3"
+                src="https://icons-for-free.com/iconfiles/png/128/sunny+weather+icon-1320196485639229368.png" alt="Clean"/>,
+    Clouds: <img width={50} height='auto' className="mr-3"
+                 src="https://icons-for-free.com/iconfiles/png/128/clouds+cloudy+weather+icon-1320196492645090832.png" alt="Clouds"/>,
+    Mist: <img width={50} height='auto' className="mr-3"
+               src="https://icons-for-free.com/iconfiles/png/128/cloud-131983802257875856.png" alt="Mist"/>,
+    Fog: <img width={50} height='auto' className="mr-3"
+              src="https://icons-for-free.com/iconfiles/png/128/fog+icon-1320185124748582180.png" alt="Fog"/>,
+    Snow: <img width={50} height='auto' className="mr-3"
+               src="https://icons-for-free.com/iconfiles/png/128/forecast+snow+weather+winter+icon-1320183300922103241.png" alt="Snow"/>,
+
+}
